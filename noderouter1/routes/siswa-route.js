f7287483c316b3fc9dd8b1e1a17e03e9600be100
endinfebryansyah <endin_febryansyah_29rpl@student.smktelkom-mlg.sch.id>
2022-05-25 20:21:46 +0700

    node router

---
+++ noderouter1/routes/siswa-route.js
@@ -0,0 +1,12 @@
+es (6 sloc)  270 Bytes
+   
+'use strict'
+
+const express = require('express')
+
+const pembayaranController = require('../controllers/pembayaran-controller')
+const router = new express.Router();
+
+router.get("/dataPembayaran/:nisn",pembayaranController.nisnPembayaran)
+
+module.exports = router
